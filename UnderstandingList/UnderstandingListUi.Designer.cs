b2133032bc3e9f148299230e750f5b7e45e29a0e
SE-MdShahadat <se.mdshahadat@gmail.com>
2020-02-21 09:53:05 +0600

    Add_Show_feature_added

---
+++ UnderstandingList/UnderstandingListUi.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace UnderstandingList
+{
+    partial class UnderstandingListUi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DescendingSortButton = new System.Windows.Forms.Button();
+            this.InsertButton = new System.Windows.Forms.Button();
+            this.AscendingSortButton = new System.Windows.Forms.Button();
+            this.OddEvenButton = new System.Windows.Forms.Button();
+            this.MaxMinButton = new System.Windows.Forms.Button();
+            this.UniqueButton = new System.Windows.Forms.Button();
+            this.DuplicateButton = new System.Windows.Forms.Button();
+            this.CopyButton = new System.Windows.Forms.Button();
+            this.SumButton = new System.Windows.Forms.Button();
+            this.ReverseButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.displayRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // DescendingSortButton
+            // 
+            this.DescendingSortButton.Location = new System.Drawing.Point(336, 267);
+            this.DescendingSortButton.Name = "DescendingSortButton";
+            this.DescendingSortButton.Size = new System.Drawing.Size(93, 27);
+            this.DescendingSortButton.TabIndex = 30;
+            this.DescendingSortButton.Text = "Sort-Des";
+            this.DescendingSortButton.UseVisualStyleBackColor = true;
+            // 
+            // InsertButton
+            // 
+            this.InsertButton.Location = new System.Drawing.Point(433, 267);
+            this.InsertButton.Name = "InsertButton";
+            this.InsertButton.Size = new System.Drawing.Size(70, 27);
+            this.InsertButton.TabIndex = 29;
+            this.InsertButton.Text = "Insert";
+            this.InsertButton.UseVisualStyleBackColor = true;
+            // 
+            // AscendingSortButton
+            // 
+            this.AscendingSortButton.Location = new System.Drawing.Point(424, 234);
+            this.AscendingSortButton.Name = "AscendingSortButton";
+            this.AscendingSortButton.Size = new System.Drawing.Size(79, 27);
+            this.AscendingSortButton.TabIndex = 28;
+            this.AscendingSortButton.Text = "Sort-Asc";
+            this.AscendingSortButton.UseVisualStyleBackColor = true;
+            // 
+            // OddEvenButton
+            // 
+            this.OddEvenButton.Location = new System.Drawing.Point(336, 234);
+            this.OddEvenButton.Name = "OddEvenButton";
+            this.OddEvenButton.Size = new System.Drawing.Size(84, 27);
+            this.OddEvenButton.TabIndex = 27;
+            this.OddEvenButton.Text = "Odd-Even";
+            this.OddEvenButton.UseVisualStyleBackColor = true;
+            // 
+            // MaxMinButton
+            // 
+            this.MaxMinButton.Location = new System.Drawing.Point(426, 201);
+            this.MaxMinButton.Name = "MaxMinButton";
+            this.MaxMinButton.Size = new System.Drawing.Size(77, 27);
+            this.MaxMinButton.TabIndex = 26;
+            this.MaxMinButton.Text = "Max-Min";
+            this.MaxMinButton.UseVisualStyleBackColor = true;
+            // 
+            // UniqueButton
+            // 
+            this.UniqueButton.Location = new System.Drawing.Point(421, 168);
+            this.UniqueButton.Name = "UniqueButton";
+            this.UniqueButton.Size = new System.Drawing.Size(82, 27);
+            this.UniqueButton.TabIndex = 25;
+            this.UniqueButton.Text = "Unique";
+            this.UniqueButton.UseVisualStyleBackColor = true;
+            // 
+            // DuplicateButton
+            // 
+            this.DuplicateButton.Location = new System.Drawing.Point(336, 201);
+            this.DuplicateButton.Name = "DuplicateButton";
+            this.DuplicateButton.Size = new System.Drawing.Size(84, 27);
+            this.DuplicateButton.TabIndex = 24;
+            this.DuplicateButton.Text = "Duplicate";
+            this.DuplicateButton.UseVisualStyleBackColor = true;
+            // 
+            // CopyButton
+            // 
+            this.CopyButton.Location = new System.Drawing.Point(336, 168);
+            this.CopyButton.Name = "CopyButton";
+            this.CopyButton.Size = new System.Drawing.Size(79, 27);
+            this.CopyButton.TabIndex = 23;
+            this.CopyButton.Text = "Copy";
+            this.CopyButton.UseVisualStyleBackColor = true;
+            // 
+            // SumButton
+            // 
+            this.SumButton.Location = new System.Drawing.Point(429, 135);
+            this.SumButton.Name = "SumButton";
+            this.SumButton.Size = new System.Drawing.Size(74, 27);
+            this.SumButton.TabIndex = 22;
+            this.SumButton.Text = "Sum";
+            this.SumButton.UseVisualStyleBackColor = true;
+            // 
+            // ReverseButton
+            // 
+            this.ReverseButton.Location = new System.Drawing.Point(336, 135);
+            this.ReverseButton.Name = "ReverseButton";
+            this.ReverseButton.Size = new System.Drawing.Size(87, 27);
+            this.ReverseButton.TabIndex = 21;
+            this.ReverseButton.Text = "Reverse";
+            this.ReverseButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Input Elements";
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(336, 104);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(167, 25);
+            this.ShowButton.TabIndex = 17;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(230, 47);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(94, 24);
+            this.AddButton.TabIndex = 16;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(141, 48);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(83, 22);
+            this.inputTextBox.TabIndex = 15;
+            // 
+            // displayRichTextBox
+            // 
+            this.displayRichTextBox.Location = new System.Drawing.Point(37, 77);
+            this.displayRichTextBox.Name = "displayRichTextBox";
+            this.displayRichTextBox.Size = new System.Drawing.Size(287, 286);
+            this.displayRichTextBox.TabIndex = 13;
+            this.displayRichTextBox.Text = "";
+            // 
+            // UnderstandingListUi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.DescendingSortButton);
+            this.Controls.Add(this.InsertButton);
+            this.Controls.Add(this.AscendingSortButton);
+            this.Controls.Add(this.OddEvenButton);
+            this.Controls.Add(this.MaxMinButton);
+            this.Controls.Add(this.UniqueButton);
+            this.Controls.Add(this.DuplicateButton);
+            this.Controls.Add(this.CopyButton);
+            this.Controls.Add(this.SumButton);
+            this.Controls.Add(this.ReverseButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Controls.Add(this.displayRichTextBox);
+            this.Name = "UnderstandingListUi";
+            this.Text = "List practice ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DescendingSortButton;
+        private System.Windows.Forms.Button InsertButton;
+        private System.Windows.Forms.Button AscendingSortButton;
+        private System.Windows.Forms.Button OddEvenButton;
+        private System.Windows.Forms.Button MaxMinButton;
+        private System.Windows.Forms.Button UniqueButton;
+        private System.Windows.Forms.Button DuplicateButton;
+        private System.Windows.Forms.Button CopyButton;
+        private System.Windows.Forms.Button SumButton;
+        private System.Windows.Forms.Button ReverseButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button ShowButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.RichTextBox displayRichTextBox;
+    }
+}
+
---
+++ UnderstandingList/UnderstandingListUi.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnderstandingList
+{
+    public partial class UnderstandingListUi : Form
+    {
+        List<int> numbers = new List<int>();
+        int index = 0;
+        string message="All elements are: ";
+        public UnderstandingListUi()
+        {
+            InitializeComponent();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            numbers.Add(Convert.ToInt32(inputTextBox.Text));
+            displayRichTextBox.Text = "Element: " + inputTextBox.Text + " , Saved!!!";
+            inputTextBox.Clear();
+            
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            foreach(int number in numbers)
+            {
+                message = message + number + " ";
+            }
+            displayRichTextBox.Text = message;
+        }
+    }
+}
